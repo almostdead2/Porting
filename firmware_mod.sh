d763483ed59ea1e24101cf37e1d7a39acae32510
almostdead2 <128363011+almostdead2@users.noreply.github.com>
2025-06-15 12:48:28 +0530

    Update firmware_mod.sh

--- firmware_mod.sh
+++ firmware_mod.sh
@@ -77,7 +77,7 @@ log_step "1" "Downloading OnePlus Firmware"
 FIRMWARE_ZIP=$(basename "$FIRMWARE_URL")
 if [[ ! -f "${FIRMWARE_DIR}/${FIRMWARE_ZIP}" ]]; then
     echo "Attempting to download firmware from: $FIRMWARE_URL"
-    wget -P "$FIRMWARE_DIR" "$FIRMWARE_URL"
+    wget --show-progress -P "$FIRMWARE_DIR" "$FIRMWARE_URL"
 else
     echo "Firmware already exists: ${FIRMWARE_DIR}/${FIRMWARE_ZIP}. Skipping download."
 fi
